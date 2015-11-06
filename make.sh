13e67c111d1dba4bc83f7b4631593a157da13ccc
Vincent <vince.rev@gmail.com>
2015-11-06 10:23:39 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,3 +4,4 @@ gfortran -std=f2008 -Wall -Wextra -O3 callback.f90 -o callback_f90
 gcc -std=c11 -Wall -Wextra -pedantic -O3 callback.c -o callback_c
 g++ -std=c++11 -Wall -Wextra -pedantic -O3 callback.cc -o callback_cc
 g++ -std=c++11 -Wall -Wextra -pedantic -O3 callback.cpp -o callback_cpp
+g++ -std=c++11 -Wall -Wextra -pedantic -O3 callback.cxx -o callback_cxx
